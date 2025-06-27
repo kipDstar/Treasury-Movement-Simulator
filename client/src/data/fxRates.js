5771b33477dd26ef0d8e14e1d4325b9e8e18dd2d
Jesse Kipsang <kipsangjesse17@gmail.com>
2025-06-28 00:50:04 +0300

    setup: populated accounts and fx rates data

---
+++ client/src/data/fxRates.js
@@ -0,0 +1,6 @@
+export const fxRates = {
+  KES: { USD: 0.007, NGN: 6.5 },
+  USD: { KES: 142, NGN: 920 },
+  NGN: { USD: 0.0011, KES: 0.15 },
+};
+// This object contains the exchange rates for KES, USD, and NGN.
